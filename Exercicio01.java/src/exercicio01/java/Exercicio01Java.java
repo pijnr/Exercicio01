8301c796b90792f9384217fdfe4a8295dfd7e9e9
pijnr <pauloivanju@gmail.com>
2017-03-20 23:42:41 -0300

    Exercicio01.java

---
+++ Exercicio01.java/src/exercicio01/java/Exercicio01Java.java
@@ -0,0 +1,14 @@
+package exercicio01.java;
+
+public class Exercicio01Java {
+
+
+    public static void main(String[] args) {
+             
+        Integer x = 2, y = 4 ;
+        Integer Soma = x+y;
+        System.out.println("A Soma é: " + Soma);
+        
+    }
+    
+}
